09aaa7cb2a30ee4aa95b05d022b4c804c3255683
iChris <christianjoseph473@gmail.com>
2022-03-27 23:55:40 -0700

    changed baseUrl

--- src/components/axiosMethod.js
+++ src/components/axiosMethod.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://iperfume.herokuapp.com/api";
-const BASE_URL = "http://localhost:8800/api";
+const BASE_URL = "https://iperfume.herokuapp.com/api";
+// const BASE_URL = "http://localhost:8800/api";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
